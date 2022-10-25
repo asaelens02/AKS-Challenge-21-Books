f682fb8a7fe6e614ac6aadc921aedc8ba02d1ddc
Ashley <ashleysaelens0@yahoo.com>
2022-10-24 21:25:30 -0500

    task 2 server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const {ApolloServer}= require ('apollo-server-express');
+const {typeDefs,resolvers}= require ('./schemas');
+const {authMiddlewar, authMiddleware}= require ('./utils/auth');
+const db = require ('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//add apollo server
+
+const server = new ApolloServer({
+  typeDefs, resolvers, context: authMiddleware
+});
+
+server.applyMiddleware ({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +27,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get ('*', (req,res)=> {
+  res.sendFile(path.join (__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
